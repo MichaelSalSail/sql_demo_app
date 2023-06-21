856cbd8f62eeaefffc671fdf517feb8870bb4d02
MichaelSalSail <msalamo000@citymail.cuny.edu>
2023-06-20 21:40:43 -0400

    Added POST functionality to web app: create new student entries and edit existing ones.

--- app.py
+++ app.py
@@ -36,3 +36,41 @@ def index():
 def student(student_id):
     student = Student.query.get_or_404(student_id)
     return render_template('student.html', student=student)
+
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        age = int(request.form['age'])
+        bio = request.form['bio']
+        student = Student(firstname=firstname,
+                          lastname=lastname,
+                          email=email,
+                          age=age,
+                          bio=bio)
+        db.session.add(student)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('create.html')
+
+@app.route('/<int:student_id>/edit/', methods=('GET', 'POST'))
+def edit(student_id):
+    student = Student.query.get_or_404(student_id)
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        age = int(request.form['age'])
+        bio = request.form['bio']
+        student.firstname = firstname
+        student.lastname = lastname
+        student.email = email
+        student.age = age
+        student.bio = bio
+        db.session.add(student)
+        db.session.commit()
+        return redirect(url_for('index'))
+
+    return render_template('edit.html', student=student)
